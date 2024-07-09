777f1e6933a29fe669bc2927e71829121a660c25
danmaraba <danielkmaraba@gmail.com>
2024-07-09 21:37:34 +0300

    iterate the links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import { links } from "./data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
+  // console.log(links);
   return (
-    <div>
-      
-    </div>
-  )
+    <nav>
+      <div className="nav-header">
+        <img
+          src="./src/assets/logo-for-delivery-company.png"
+          alt="logo"
+          className="logo"
+        />
+        <button className="toggle-btn">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+      <div className="links-container">
+        <ul className="links">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
