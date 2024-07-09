eed49ac69f3e2276893743bf805fc87cade9a866
danmaraba <danielkmaraba@gmail.com>
2024-07-09 23:36:07 +0300

    add the useffect hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { links } from "./data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
+    const[showLink, setShowLink]=useState(false)
   // console.log(links);
   return (
     <nav>
