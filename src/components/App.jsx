70dea5d4c9bc845bd9562592b6d6f6a49213ae39
danmaraba <danielkmaraba@gmail.com>
2024-07-12 15:35:11 +0300

    import Routes and Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import '../App.css'
+import { Routes,Route} from 'react-router-dom';
 import Navbar from './Navbar'
+// import Home from './Home';
+import Login from './Login'
+// import About from './About'
+// import Services from './Services'
+// import Contact from './Contact'
 
 function App() {
 
   return (
-    <>
+    <div>
       <Navbar/>
-    </>
+      <Routes>
+
+        {/* <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/contacts' element={<Contact/>}/> */}
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      {/* <Navbar/> */}
+    </div>
   )
 }
 
