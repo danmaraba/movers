23c83cf93bee60221d15264a8aff4467ddba7f93
danmaraba <danielkmaraba@gmail.com>
2024-07-15 15:35:57 +0300

    import components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 import '../App.css'
 import { Routes,Route} from 'react-router-dom';
-import Navbar from './Navbar'
-// import Home from './Home';
+import Navbar from './Home'
+import Home from './Home';
 import Login from './Login'
-// import About from './About'
-// import Services from './Services'
-// import Contact from './Contact'
+import About from './About'
+import Services from './Services'
+import Contacts from './Contacts'
+import Navlinks from './Navlinks';
 
 function App() {
 
   return (
     <div>
-      <Navbar/>
+      <Navlinks/>
       <Routes>
-
-        {/* <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/services' element={<Services/>}/>
-        <Route path='/contacts' element={<Contact/>}/> */}
+        <Route path='/contacts' element={<Contacts/>}/>
         <Route path='/login' element={<Login/>}/>
       </Routes>
-      {/* <Navbar/> */}
     </div>
   )
 }
