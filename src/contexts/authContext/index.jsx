aa0cd2610e0784db224dfc7fc77966ea3edadcc2
danmaraba <danielkmaraba@gmail.com>
2024-07-16 22:56:19 +0300

    create useeffect

--- src/contexts/authContext/index.jsx
+++ src/contexts/authContext/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { auth } from "../../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext=React.createContext();
 
@@ -7,4 +8,9 @@ export function AuthProvider({children}){
    const[currentUser,setCurrentUser]= useState(null);
    const[userLoggedIn, setUserLoggedIn]=useState(false);
    const[loading,setLoading]=useState(true)
+
+   useEffect(()=>{
+    const unsubscribe=onAuthStateChanged(auth,initializeUser)
+    return unsubscribe
+   },[])
 }
