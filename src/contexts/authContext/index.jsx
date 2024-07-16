7aa87790735886e9249c6b074f51093ebbc54739
danmaraba <danielkmaraba@gmail.com>
2024-07-16 22:52:30 +0300

    create context

---
+++ src/contexts/authContext/index.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { auth } from "../../firebase/firebase";
+
+const AuthContext=React.createContext();
+
+export function AuthProvider({children}){
+   const[currentUser,setCurrentUser]= useState(null);
+   const[userLoggedIn, setUserLoggedIn]=useState(false);
+   const[loading,setLoading]=useState(true)
+}
