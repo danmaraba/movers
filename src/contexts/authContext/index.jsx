2d441677f810c3018761ea600583f50f0dc34f69
danmaraba <danielkmaraba@gmail.com>
2024-07-16 23:09:34 +0300

    create value

--- src/contexts/authContext/index.jsx
+++ src/contexts/authContext/index.jsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { useEffect,useContext,useState } from "react";
 import { auth } from "../../firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext=React.createContext();
+
+export function useAuth(){
+    return useContext(AuthContext)
+}
 
 export function AuthProvider({children}){
    const[currentUser,setCurrentUser]= useState(null);
@@ -22,5 +26,16 @@ export function AuthProvider({children}){
         setCurrentUser(null)
         setUserLoggedIn(false)
     }
+    setLoading(false)
    }
+   const value={
+    currentUser,
+    userLoggedIn,
+    loading
+   }
+   return(
+    <AuthContext.Provider value={value}>
+        {!loading &&children}
+    </AuthContext.Provider>
+   )
 }
