0c2ad9754aff4630f7ce5ed5ff9724574d78f425
danmaraba <danielkmaraba@gmail.com>
2024-07-18 15:48:45 +0300

    add richcolors to the toaster component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import '../App.css'
 import { Routes,Route} from 'react-router-dom';
-import Navbar from './Home'
-import Home from './Home';
+// import Navbar from './Home'
+// import Home from './Home';
 import Login from './Login'
 import About from './About'
-import Services from './Services'
-import Contacts from './Contacts'
-import Navlinks from './Navlinks';
+// import Services from './Services'
+// import Contacts from './Contacts'
+// import Navlinks from './Navlinks';
+import Register from './Register';
+// import Distance from './Distance';
+import { Toaster } from 'sonner';
 
 function App() {
 
   return (
     <div>
-      <Navlinks/>
+      {/* <Distance/> */}
+      {/* <Navlinks/> */}
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        {/* <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/services' element={<Services/>}/>
-        <Route path='/contacts' element={<Contacts/>}/>
+        <Route path='/contacts' element={<Contacts/>}/> */}
         <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
+      <Toaster richColors/>
     </div>
   )
 }
