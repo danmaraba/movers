6589e1bf7a1efc9c1e0eee885c1261d0fff8a810
danmaraba <danielkmaraba@gmail.com>
2024-07-18 20:08:11 +0300

    create a useeffect

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,36 @@
-import React,{useState} from 'react'
-import { auth } from '../firebase/firebase'
+import React,{useEffect, useState} from 'react'
+import { auth,db } from '../firebase/firebase'
 import { toast } from 'sonner'
+import { doc,getDoc } from 'firebase/firestore'
 
 function Profile() {
-    const[userDetails,setUserDetails]=useState()
+    const[userDetails,setUserDetails]=useState(null)
+    const fetchUserData=async()=>{
+        auth.onAuthStateChanged(async(user)=>{
+            console.log(user);
+        })
+    }
+    useEffect(()=>{
+        fetchUserData()
+    },[])
   return (
     <div>
-      <h1>Welcome</h1>
+        {
+            userDetails?(
+                <>
+                <h3>Welcome {userDetails.firstName}.</h3>
+                <div>
+                    <p>Email:{userDetails.email}</p>
+                    <p>firstName:{userDetails.firstName}</p>
+                    <p>lastName:{userDetails.lastName}</p>
+                </div>
+                <button className="logout-btn">
+                    Logout
+                </button>
+                </>
+            )
+            :(<p>Loading...</p>)
+        }
     </div>
   )
 }
