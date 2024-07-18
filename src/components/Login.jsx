6fd3c93da658ae7f2bbc65ed5a56148ed99c2f14
danmaraba <danielkmaraba@gmail.com>
2024-07-18 11:08:38 +0300

    create a form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,67 @@
 import React,{useState}from "react";
+import { useAuth } from "../contexts/authContext";
+import { doSignInWithEmailAndPassword} from "../firebase/auth";
+import { Navigate } from "react-router-dom";
 function Login() {
    const[action, setAction]= useState("Login")
+   const[email,setEmail]=useState('')
+   const[password,setPassword]=useState('')
+   const[isSigningIn, setIsSigningIn]=useState(false);
+  //  const[errorMessage,setErrorMessage]=useState('')
+   const{userLoggedIn}=useAuth()
 
-   function handleSubmit(e){
+   const handleSubmit=async (e)=>{
     e.preventDefault()
+    if(!isSigningIn){
+      setIsSigningIn(true)
+     await doSignInWithEmailAndPassword(email,password)
+    }
    }
+
+  //  const onGoogleSignIn=(e)=>{
+  //   e.preventDefault()
+  //   if(!isSigningIn){
+  //     setIsSigningIn(true)
+  //     doSignInWithGoogle.catch(err=>{
+  //       setIsSigningIn(false)
+  //     })
+  //   }
+  //  }
   return (
-    <div className="container">
-        
-      <form action="" onSubmit={handleSubmit}>
-        <div className="text">{action}</div>
-        <div className="underline"></div>
-        {action==='Login'?<div></div>:<div className="input">
-          <label htmlFor="username">Username</label>
-          <input type="text" />
-        </div>}
-        <div className="input">
-          <label htmlFor="email">Email</label>
-          <input type="email" />
-        </div>
-        <div className="input">
-          <label htmlFor="password">Password</label>
-          <input type="password" />
-        </div>
-        {action==='Sign Up'?<div></div>:<div className="forgot-password">
-            Forgot Password? <span><a href="">Click Here</a></span>
-        </div>}
-        <div className="login-btns">
-        <button className={action==="Login"?"sign-up gray":"sign-up"} onClick={()=>{setAction('Sign Up')}}>Sign Up</button>
-        <button className={action==="Sign Up"?"sign-up gray":"sign-up"}onClick={()=>{setAction('Login')}}>Login</button>
-        </div>
-      </form>
-    </div>
+    <form action="">
+      <h3>Login</h3>
+      <div className="login-email">
+        <label >Email address</label>
+        <input type="email" className="form-control" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      </div>
+    </form>
+    
+    // <div className="container">
+    //   {userLoggedIn && (<Navigate to={'/home'} replace={true}/>)}
+    //   <form action="" onSubmit={handleSubmit}>
+    //     <div className="text">{action}</div>
+    //     <div className="underline"></div>
+    //     {action==='Login'?<div></div>:<div className="input">
+    //       <label htmlFor="username">Username</label>
+    //       <input type="text" />
+    //     </div>}
+    //     <div className="input">
+    //       <label htmlFor="email">Email</label>
+    //       <input type="email" />
+    //     </div>
+    //     <div className="input">
+    //       <label htmlFor="password">Password</label>
+    //       <input type="password" />
+    //     </div>
+    //     {action==='Sign Up'?<div></div>:<div className="forgot-password">
+    //         Forgot Password? <span><a href="">Click Here</a></span>
+    //     </div>}
+    //     <div className="login-btns">
+    //     <button className={action==="Login"?"sign-up gray":"sign-up"} onClick={()=>{setAction('Sign Up')}}>Sign Up</button>
+    //     <button className={action==="Sign Up"?"sign-up gray":"sign-up"}onClick={()=>{setAction('Login')}}>Login</button>
+    //     </div>
+    //   </form>
+    // </div>
   );
 }
 
