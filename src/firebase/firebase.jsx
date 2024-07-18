7c5ddf22b67d69c202e2866e50a7705fa109279b
danmaraba <danielkmaraba@gmail.com>
2024-07-18 14:30:32 +0300

    import firestore

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,9 +18,11 @@ const firebaseConfig = {
   measurementId: "G-FW2Q7F3225"
 };
 
+// get auth is a method provided by the Firebase SDKs (such as Firebase JavaScript SDK) that allows you to retrieve the current authentication state of a user
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth=getAuth(app)
+export const db=getFirestore(app)
 
 export {app,auth};
