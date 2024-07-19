103a90e878defd127ce0de362a34dc6007446f92
danmaraba <danielkmaraba@gmail.com>
2024-07-19 08:55:52 +0300

    import components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import '../App.css'
 import { Routes,Route} from 'react-router-dom';
 // import Navbar from './Home'
-// import Home from './Home';
+import Home from './Home';
 import Login from './Login'
 import About from './About'
-// import Services from './Services'
-// import Contacts from './Contacts'
-// import Navlinks from './Navlinks';
+import Services from './Services'
+import Contacts from './Contacts'
+import Navlinks from './Navlinks';
 import Register from './Register';
 // import Distance from './Distance';
 import { Toaster } from 'sonner';
@@ -21,12 +21,12 @@ function App(){
   return (
     <div>
       {/* <Distance/> */}
-      {/* <Navlinks/> */}
+      <Navlinks/>
       <Routes>
-        {/* <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/services' element={<Services/>}/>
-        <Route path='/contacts' element={<Contacts/>}/> */}
+        <Route path='/contacts' element={<Contacts/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/profile' element={<Profile/>}/>
