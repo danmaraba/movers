9ae743c998b8132312e1cc3b6ea90705bcd6d469
danmaraba <danielkmaraba@gmail.com>
2024-07-19 08:54:29 +0300

    change the image width

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,11 +8,12 @@ function Profile() {
     const fetchUserData=async()=>{
         auth.onAuthStateChanged(async(user)=>{
             console.log(user);
+            setUserDetails(user)
             const docRef=doc(db,"users",user.uid)
             const docSnap=await getDoc(docRef)
             if(docSnap.exists()){
                 setUserDetails(docSnap.data())
-                console.log(docSnap.data);
+                console.log(docSnap.data());
             }else{
                 console.log("user is not logged in");
             }
@@ -26,21 +27,24 @@ function Profile() {
         try {
             await auth.signOut()
             window.location.href="/login"
-            console.log("User logged out successfully!");
+            toast.success("User logged out successfully!");
         } catch (error) {
             console.log("Error logging out:",error.message);
         }
     }
   return (
-    <div>
+    <div className='profile-div'>
         {
             userDetails?(
                 <>
-                <h3>Welcome {userDetails.firstName}.</h3>
-                <div>
+                <div style={{display:"flex",justifyContent:"center"}}>
+                    <img src={userDetails.photo}  width={"60%"} style={{borderRadius:"50%"}}/>
+                </div>
+                <h3>Welcome <span id='userdetails'>{userDetails.firstName}.</span></h3>
+                <div className='profile-details'>
                     <p>Email:{userDetails.email}</p>
-                    <p>firstName:{userDetails.firstName}</p>
-                    <p>lastName:{userDetails.lastName}</p>
+                    <p>Name:{userDetails.firstName}</p>
+                    {/* <p>lastName:{userDetails.lastName}</p> */}
                 </div>
                 <button className="logout-btn" onClick={handleLogOut}>
                     Logout
