af9be8b695737735b4b34d0da10db413179c457d
danmaraba <danielkmaraba@gmail.com>
2024-07-20 20:07:47 +0300

    add textdecoration

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,25 +8,25 @@ import {
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
 import { faTiktok, faInstagram } from "@fortawesome/free-brands-svg-icons";
-import{NavLink,Link} from 'react-router-dom'
+import { NavLink, Link } from "react-router-dom";
 import Testimonials from "./Testimonials";
 
 function Navbar() {
   const linkRef = useRef(null);
   const linksContainerRef = useRef(null);
   const [showLinks, setShowLinks] = useState(false);
-  const home=useRef(null)
-  const about=useRef(null)
-  const services=useRef(null)
-  const contacts=useRef(null)
-    const scrollToSection = (elementRef) => {
-        window.scrollTo({
-          top: elementRef.current.offsetTop,
-          behavior: "smooth",
-        });
-      };
- // get the current year
- const date=new Date().getFullYear()
+  const home = useRef(null);
+  const about = useRef(null);
+  const services = useRef(null);
+  const contacts = useRef(null);
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+  // get the current year
+  const date = new Date().getFullYear();
 
   useEffect(() => {
     const linksHeight = linkRef.current.getBoundingClientRect().height;
@@ -39,20 +39,20 @@ function Navbar() {
   }, [showLinks]);
   // console.log(links);
 
-  const navbar=document.getElementById('navbar')
+  const navbar = document.getElementById("navbar");
   const handleScroll = () => {
-      if(window.scrollY===0){
-        navbar.style.boxShadow='0 5px 15px rgba(0, 0, 0, 0.1)';
-      }else{
-        navbar.style.boxShadow='none';
-      }
+    if (window.scrollY === 0) {
+      navbar.style.boxShadow = "0 5px 15px rgba(0, 0, 0, 0.1)";
+    } else {
+      navbar.style.boxShadow = "none";
+    }
   };
-  window.addEventListener('scroll', handleScroll);
-  
+  window.addEventListener("scroll", handleScroll);
+
   return (
     <>
-      <header >
-        <nav id="navbar"ref={home}>
+      <header>
+        <nav id="navbar" ref={home}>
           <div className="nav-center">
             <div className="nav-header">
               <img
@@ -71,43 +71,61 @@ function Navbar() {
             </div>
             <div className="links-container" ref={linksContainerRef}>
               <ul className="links" ref={linkRef}>
-                  <li onClick={()=>{
-                    scrollToSection(home)
-                  }}><Link to="/">Home</Link></li>
-                  <li onClick={()=>{
-                    scrollToSection(about)
-                  }}><Link to="/about">About</Link></li>
-                  <li onClick={()=>{
-                    scrollToSection(services)
-                  }}><Link to="/services">Services</Link></li>
-                  <li onClick={()=>{
-                    scrollToSection(contacts)
-                  }}><Link to="/contacts">Contacts</Link></li>
-                  <li id="login-link"><Link to="/login">Login</Link></li>
+                <li
+                  onClick={() => {
+                    scrollToSection(home);
+                  }}
+                >
+                  <Link to="/">Home</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    scrollToSection(about);
+                  }}
+                >
+                  <Link to="/about">About</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    scrollToSection(services);
+                  }}
+                >
+                  <Link to="/services">Services</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    scrollToSection(contacts);
+                  }}
+                >
+                  <Link to="/contacts">Contacts</Link>
+                </li>
+                <li id="login-link">
+                  <Link to="/login">Login</Link>
+                </li>
               </ul>
             </div>
           </div>
         </nav>
       </header>
       {/* about */}
-      <section className="about"ref={about} >
+      <section className="about" ref={about}>
         <h1>About Us</h1>
-      <div className="abt">
-        <img
-          src="./src/assets/pexels-tima-miroshnichenko-6169668.jpg"
-          alt="img"
-          className="img"
-        />
-        <p>
-          We are a logistic business with and primarily deal with car hire
-          service and parcel delivery. We pride ourselves with exceptional
-          services that we offer to our customers. Your safety and those of your
-          goods is our top priority. Order either of the services below and let
-          us get into business!
-        </p>
-      </div>
-    </section>
-     
+        <div className="abt">
+          <img
+            src="./src/assets/pexels-tima-miroshnichenko-6169668.jpg"
+            alt="img"
+            className="img"
+          />
+          <p>
+            We are a logistic business with and primarily deal with car hire
+            service and parcel delivery. We pride ourselves with exceptional
+            services that we offer to our customers. Your safety and those of
+            your goods is our top priority. Order either of the services below
+            and let us get into business!
+          </p>
+        </div>
+      </section>
+
       {/* services */}
       <section className="services" ref={services}>
         <h1>Our Services</h1>
@@ -120,8 +138,13 @@ function Navbar() {
             />
             <div className="card-content">
               <h4>parcel delivery</h4>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, magni?</p>
-              <NavLink to="/login" className="btn">order delivery</NavLink>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde,
+                magni?
+              </p>
+              <NavLink to="/login" className="btn">
+                order delivery
+              </NavLink>
             </div>
           </div>
           <div className="card">
@@ -132,16 +155,21 @@ function Navbar() {
             />
             <div className="card-content">
               <h4>car hire</h4>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, magni?</p>
-              <NavLink to="/login" className="btn">hire car</NavLink>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde,
+                magni?
+              </p>
+              <NavLink to="/login" className="btn">
+                hire car
+              </NavLink>
             </div>
           </div>
         </div>
       </section>
-      <Testimonials/>
+      <Testimonials />
       {/* contacts */}
       <section className="contacts-footer-section">
-        <p>
+        <p className="heading">
           We are here to facilitate safe and seamless delivery of your parcel!
         </p>
         <div className="contact-details-section">
@@ -154,16 +182,16 @@ function Navbar() {
             <div className="contact-links">
               <ul>
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/" style={{textDecoration:"none"}}>Home</Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link to="/about" style={{textDecoration:"none"}}>About</Link>
                 </li>
                 <li>
-                  <Link to="/services">Services</Link>
+                  <Link to="/services" style={{textDecoration:"none"}}>Services</Link>
                 </li>
                 <li>
-                  <Link to="/contacts">Contacts</Link>
+                  <Link to="/contacts" style={{textDecoration:"none"}}>Contacts</Link>
                 </li>
               </ul>
             </div>
